ef8ce39a0dbd0202d843a3883f3f5e9702fb9c59
Akshay Pai <sadypai@gmail.com>
2020-12-08 15:34:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,82 @@
+// status flag to yield a running generator
 let status = false;
 
-const gen1 = document.getElementById("gen1");
-const progressGen1 = document.getElementById("progressGen1");
-const gen2 = document.getElementById("gen2");
-const progressGen2 = document.getElementById("progressGen2");
-const gen3 = document.getElementById("gen3");
-const progressGen3 = document.getElementById("progressGen3");
-const gen4 = document.getElementById("gen4");
-const progressGen4 = document.getElementById("progressGen4");
-
-async function* gen(counter, progress) {
-  counter.parentElement.classList.remove("init");
-  let i = 0;
-  while (i < 30) {
-    await new Promise((resolve) => setTimeout(resolve, 400));
-    counter.innerText = ++i;
-    progress.value = i;
-    counter.parentElement.classList.add("active");
-    if (status) {
-      counter.parentElement.classList.remove("active");
-      yield;
-    }
-  }
-  counter.parentElement.classList.remove("active");
-  counter.parentElement.classList.add("complete");
-}
-
-const iteratorQueue = [gen(gen1, progressGen1), gen(gen2, progressGen2), gen(gen3, progressGen3), gen(gen4, progressGen4)];
-let iterator;
-
-async function startGenerators() {
-  while (iteratorQueue.length !== 0) {
-    iterator = iteratorQueue.shift();
-    const value = await iterator.next();
-    if (!value.done) iteratorQueue.push(iterator);
-    status = false;
-  }
-}
-
+// Start generators
 document.getElementById("start").addEventListener("click", function () {
   this.style.display = "none";
   startGenerators();
 });
 
+// Yield a generator
 document.getElementById("yield").addEventListener("click", () => {
   status = true;
 });
+
+// Update the counter
+function updateElementWithCount(element, value) {
+  element.textContent = value;
+  element.value = value;
+}
+
+function markStart(parentElement) {
+  parentElement.classList.remove("init");
+}
+
+function markComplete(parentElement) {
+  parentElement.classList.remove("active");
+  parentElement.classList.add("complete");
+}
+
+function markActive(parentElement) {
+  parentElement.classList.add("active");
+}
+
+function markInactive(parentElement) {
+  parentElement.classList.remove("active");
+}
+
+/** Core logic starts here **/
+
+// Generator for counter
+async function* counterGenerator(counterElement, progressElement) {
+  const parentElement = counterElement.parentElement;
+  markStart(parentElement);
+
+  let count = 0;
+  while (count < 30) {
+    markActive(parentElement);
+
+    // delay by 0.4 secs
+    await new Promise((resolve) => setTimeout(resolve, 400));
+    count = count + 1;
+
+    updateElementWithCount(counterElement, count);
+    updateElementWithCount(progressElement, count);
+
+    if (status) {
+      markInactive(parentElement);
+      yield;
+    }
+  }
+  markComplete(parentElement);
+}
+
+// Iterator queue to execute in sequence
+const iteratorQueue = [
+  counterGenerator(document.getElementById("counter1"), document.getElementById("progress1")),
+  counterGenerator(document.getElementById("counter2"), document.getElementById("progress2")),
+  counterGenerator(document.getElementById("counter3"), document.getElementById("progress3")),
+  counterGenerator(document.getElementById("counter4"), document.getElementById("progress4")),
+];
+
+// iterator will be executed and on yield will be moved to end of the queue
+async function startGenerators() {
+  while (iteratorQueue.length !== 0) {
+    const iterator = iteratorQueue.shift();
+    const value = await iterator.next();
+    if (!value.done) {
+      iteratorQueue.push(iterator);
+    }
+    status = false;
+  }
+}
